40267b55ea2ed2ff421e298e80d3bf4acacfbc6b
Alyx Faber <alyx.faber@student.csulb.edu>
2021-02-22 23:25:53 -0800

    2.0

---
+++ js/ex2.js
@@ -0,0 +1,14 @@
+// a.	An array of odd numbers
+// b.	An array of numbers divisible by 2 or 5
+// c.	An array of numbers divisible by 3 squared
+// d.	The sum of the following: square the numbers divisible by 5
+
+const my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(`An array of odd numbers are: `)
+console.log(`An array of numbers divisble by 2 or 5 are: `)
+console.log(`An array of numbers divisible by 3 squared are: `)
+console.log(`The sum of the following: square the numnbers divisible by 5 are: `)
+
+
+
